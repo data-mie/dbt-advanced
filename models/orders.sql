3da417e03526d8c06acccf9d881b2ac4e236ae8b
Raquel Simão <raquel.simao@siemens.com>
2025-01-28 14:56:02 +0000

    Lab 6 done in orders.sql

--- models/orders.sql
+++ models/orders.sql
@@ -1,8 +1,22 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        schema_change ='sync_all_columns'
+
+    )
+}}
+
+-- you can start writing _config and click enter on _config_incremental
+-- add  schema_change ='sync_all_columns' to make sure if the columns name change, it does not give an error
+
 with orders as (
-    select stg.*, map.store_name
+    select stg.*,
+    map.store_name
     from {{ ref('stg_ecomm__orders') }} as stg
     left join {{ref('stores')}} as map 
     on stg.store_id = map.store_id
+
 ),
 
 -- or i could just add this table
@@ -29,7 +43,6 @@ joined as (
         orders.order_status,
         orders.total_amount,
         orders.store_name,
-        -- store.store_name instead of orders.store_name if i added the with store()
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -38,6 +51,7 @@ joined as (
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
         ) as delivery_time_from_collection
+        -- store.store_name instead of orders.store_name if i added the with store()
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
@@ -51,3 +65,22 @@ final as (
 
 select *
 from final
+{% if is_incremental() %}
+-- this filter will only be applied on an incremental run
+    where ordered_at > (select dateadd('day', -3, max(ordered_at)) from {{ this }}) 
+{% endif %}
+
+
+-- Some orders to arrive  much later than they should. As a result, we're now worried about 
+--our incremental model missing data. We want to check how many days back we need to inspect 
+--what the typical difference is between the two columns:
+
+--We run in snowflake :
+
+    --select
+    --datediff('day', created_at, _synced_at) as days_lag,
+    --count(*)
+    --from raw.ecomm.orders
+    --group by 1
+
+ --then we checked 3 days was enough */
