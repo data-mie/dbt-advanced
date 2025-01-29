9b271d71dbffa97c15f6722d9aabbb34ff1d4ad6
Rui Malta <rui.porfirio_rodrigues_malta@siemens.com>
2025-01-29 14:56:17 +0000

    dbt lab 10 1st case

--- models/transform/trf_customers.sql
+++ models/transform/trf_customers.sql
@@ -56,9 +56,13 @@ joined as (
 ),
 
 final as (
-    select *
+    select {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as pk_customers, 
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as hk_customers, *, current_timestamp() as last_updated,
+    greatest_ignore_nulls(
+        most_recent_order_at, created_at, survey_date::timestamp
+    ) as source_last_updated
     from joined
 )
 
 select *
-from final
+from final 
