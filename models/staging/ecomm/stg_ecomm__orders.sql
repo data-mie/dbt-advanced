3f656e0643183fded87d6b982209a8ca7f7b9746
Rui Malta <rui.porfirio_rodrigues_malta@siemens.com>
2025-01-29 11:31:45 +0000

    final stg ecomm orders

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -9,12 +9,19 @@ with source as (
 
 renamed as (
     select
-        *,
+        total_amount, customer_id, _synced_at,
         id as order_id,
         created_at as ordered_at,
-        status as order_status
+        status as order_status,
+        case when _dbt_source_relation ilike '%orders_us' then '1'
+        when _dbt_source_relation ilike '%orders_au' then '2'
+        when _dbt_source_relation ilike '%orders_de' then '3' end as store_id,
+        case when _dbt_source_relation ilike '%orders_us' then 'USD'
+        when _dbt_source_relation ilike '%orders_au' then 'AUD'
+        when _dbt_source_relation ilike '%orders_de' then 'EUR' end as currency
     from source
 ),
+
 
 final as (
     select *
