38166909b8f6934d24c8f78b10ef9f8beed60fc3
Simo Tumelius <simo@datamie.fi>
2025-03-06 10:34:47 +0000

    Add order status normalization to stg_ecomm__orders.sql

--- models/staging/stg_ecomm__orders.sql
+++ models/staging/stg_ecomm__orders.sql
@@ -12,9 +12,23 @@ renamed as (
     from source
 ),
 
+normalize_order_status as (
+    select
+        *,
+        case 
+            when order_status ilike any('ordered', 'order_created') then 'Ordered'
+            when lower(order_status) in ('shipped', 'sent') then 'Shipped'
+            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
+            when order_status = 'canceled' or  order_status = 'cancelled' then 'Canceled'
+            when order_status = 'delivered' then 'Delivered'
+            else 'Unknown'
+        end as order_status_normalized
+    from renamed
+),
+
 final as (
     select *
-    from renamed
+    from normalize_order_status
 )
 
 select *
