8e1180af4f767d7d4e8275a17024eecc26f8fb11
Marvin Geerken <marvin.geerken@hunseranalytics.com>
2025-03-09 12:38:57 +0000

    lab test finish 6

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,6 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        on_schema_change='sync_all_columns'
+    )
+}}
+
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
+{% if is_incremental() %}
+    where ordered_at >= (select dateadd('day', -3, max(ordered_at)) from {{ this }})
+{% endif %}
+
 ),
 
 deliveries as (
@@ -43,7 +55,9 @@ joined as (
 ),
 
 final as (
-    select *
+    select
+        *,
+        current_timestamp() as inserted_at
     from joined
 )
 
