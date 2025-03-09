aa04750cdacf813aea97a1eb74bd6998eeab4c95
Marvin Geerken <marvin.geerken@hunseranalytics.com>
2025-03-09 16:39:37 +0000

    lab 8 test semi finished

--- models/staging/stg_ecomm__orders.sql
+++ models/staging/stg_ecomm__orders.sql
@@ -1,11 +1,24 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
 ),
 
-order_status as (
-    select *
-    from {{ ref('order_status') }}
+add_store_id as (
+    select
+        * exclude (store_id),
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id
+    from sources
 ),
 
 renamed as (
@@ -14,23 +27,19 @@ renamed as (
         id as order_id,
         created_at as ordered_at,
         status as order_status
-    from source
+    from add_store_id
 ),
 
-normalize_order_status as (
-    select
-        renamed.*,
-        coalesce(order_status.order_status_normalized, 'Unknown') as order_status_normalized
-    from renamed
-    left join order_status on (
-        lower(renamed.order_status) = order_status.order_status
-    )
-),
-
-final as (
-    select *
-    from normalize_order_status
+deduplicated as (
+    {{
+        dbt_utils.deduplicate(
+            relation='renamed',
+            partition_by='order_id',
+            order_by='_synced_at desc'
+        )
+    }}
 )
 
-select *
-from final
+select
+    *
+from deduplicated
