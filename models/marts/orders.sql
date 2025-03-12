24413c2e65c4f2593dec68145c4c2ef07e6d4130
Simo Tumelius <simo@datamie.fi>
2025-03-12 08:45:40 +0000

    Complete lab 6!

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,6 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        on_schema_change='append_new_columns'
+    )
+}}
+
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where ordered_at > (select max(ordered_at) - interval '3 days' from {{ this }}) 
+    {% endif %}
 ),
 
 deliveries as (
@@ -43,7 +55,9 @@ joined as (
 ),
 
 final as (
-    select *
+    select
+        *,
+        current_timestamp() as last_updated
     from joined
 )
 
