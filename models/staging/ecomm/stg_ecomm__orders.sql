15a9af8ff343a4557bfa254b070c80b7c5fcef0b
Simo Tumelius <simo@datamie.fi>
2025-03-12 10:47:23 +0000

    Complete Lab 9!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,12 +1,29 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
 ),
-
 
 order_status as (
     select *
     from {{ ref('order_status') }}
+),
+
+add_store_id as (
+    select
+        * exclude (store_id),    -- Omit original store_id column
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id            -- Add calculated store_id
+    from sources
 ),
 
 renamed as (
@@ -14,7 +31,7 @@ renamed as (
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
@@ -27,9 +44,19 @@ normalize_order_status as (
     )
 ),
 
+deduplicated as (
+    {{
+        dbt_utils.deduplicate(
+            relation='normalize_order_status',
+            partition_by='order_id',
+            order_by='_synced_at desc'
+        )
+    }}
+),
+
 final as (
     select *
-    from normalize_order_status
+    from deduplicated
 )
 
 select *
