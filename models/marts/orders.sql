96ca9a9fe4fccd4a45cc9ede029e85b595977846
Robert Krebs <robert.krebs@siemens.com>
2025-03-13 14:22:35 +0000

    Lab 6 completed!

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,6 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        on_schema_change='append_new_columns'
+    )
+}}
+
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where ordered_at >= (select dateadd('day', -3, max(ordered_at)) from {{ this }}) 
+    {% endif %}
 ),
 
 deliveries as (
@@ -34,7 +46,8 @@ joined as (
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
         ) as delivery_time_from_collection,
-        store_name.store_name
+        store_name.store_name,
+        current_timestamp() as last_updated
 
     from orders
     left join deliveries_filtered
