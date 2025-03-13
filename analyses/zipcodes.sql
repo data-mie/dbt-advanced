72389e1b1f12b54d8e8aa749381a9ca48a24d126
João Pedro Enes Barbosa <joao-pedro@siemens.com>
2025-03-13 16:56:50 +0000

    lab7

--- analyses/zipcodes.sql
+++ analyses/zipcodes.sql
@@ -1,8 +1,8 @@
 select 
     country,
     state.value['state']::varchar as state,
-    zipcodes.value:zipcode::varchar as zip_code,
-    zipcodes.value:city::varchar as city
+    zipcodes.value['zipcode']::varchar as zip_code,
+    zipcodes.value['city']::varchar as city
 from raw.geo.countries,
 lateral flatten(input => states ) as state,
-lateral flatten(input => state.value:zipcodes) as zipcodes
+lateral flatten(input => state.value['zipcodes'] as zipcodes
