71142486f04b608fec6f9224085a285b1e134c8f
Athanasia Giatsou <athanasia.giatsou@siemens.com>
2025-03-13 11:00:51 +0000

    lab 5: snapshots and seeds

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -14,6 +14,11 @@ deliveries_filtered as (
     where delivery_status = 'delivered'
 ),
 
+stores as (
+    select *
+    from {{ ref('stores') }}
+),
+
 joined as (
     select
         orders.order_id,
@@ -21,6 +26,7 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        stores.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -32,6 +38,8 @@ joined as (
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
+    left join stores
+        on orders.store_id = stores.store_id
 ),
 
 final as (
