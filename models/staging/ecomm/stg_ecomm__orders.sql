3e5f4f64b4ec84632acf26ce0a07511e6112316e
Ana Frias <ana.frias@siemens.com>
2025-03-13 10:58:48 +0000

    lab 5

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -12,29 +12,27 @@ renamed as (
     from source
 ),
 
+store_name as (
+select * from {{ ref('stores') }}
+),
+
+
 normalize_order_status as (
     select
-        *,
-        -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+        renamed.*,
+        coalesce(order_status.order_status_normalized, 'Unknown') as order_status
     from renamed
+    left join order_status on (
+        lower(renamed.status) = order_status.order_status
+    )
 ),
 
 final as (
     select *
-    from normalize_order_status
+    from normalize_order_status nor
+    inner join store_name sto
+    on nor.store_id = sto.store_id
 )
-
+--345 rows
 select *
 from final
---
+++ snapshots/orders_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot orders_snapshot %}
+
+{{
+      config(
+            schema='snapshots',
+            unique_key='id',
+            strategy='timestamp',
+            updated_at = '_synced_at',
+      )
+}}
+
+select
+    *
+from {{ source('ecomm', 'orders') }}
+
+{% endsnapshot %}
