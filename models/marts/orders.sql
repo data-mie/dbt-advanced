f89b6902f669f61a3ff6dcbf107c33e4e2ab25e8
João Pedro Enes Barbosa <joao-pedro@siemens.com>
2025-03-13 14:45:55 +0000

    lab6

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -9,9 +9,6 @@
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
-    {% if is_incremental() %}
-        where ordered_at > (select dateadd(day, -3 , max(ordered_at) ) from {{ this }}) 
-    {% endif %}
 ),
 
 deliveries as (
@@ -49,6 +46,9 @@ final as (
     select * , 
            current_timestamp as last_updated
     from joined
+        {% if is_incremental() %}
+        where ordered_at > (select dateadd(day, -3 , max(ordered_at) ) from {{ this }}) 
+    {% endif %}
 )
 
 select *
