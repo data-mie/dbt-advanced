d830f51dc1152effd6163896f569707468f2ca3a
Athanasia Giatsou <athanasia.giatsou@siemens.com>
2025-03-14 16:00:23 +0000

    Lab 10 completed!

--- models/docs.md
+++ models/docs.md
@@ -1,3 +1,11 @@
 {% docs customer_id %}
 A unique customer ID from our ecommerce application.
 {% enddocs %}
+
+{% docs source_last_updated %}
+Latest ingestion timestamp from all included sources per record
+{% enddocs %}
+
+{% docs last_updated %}
+Current timestamp of the table/record creation
+{% enddocs %}
--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,13 +1,20 @@
 {{
     config(
         materialized='incremental',
-        unique_key='order_id',
+        unique_key='pk_orders',
         on_schema_change='append_new_columns'
     )
 }}
 
 with orders as (
-    select *
+    select
+        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as pk_orders,
+        {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as hk_customer,
+        ordered_at,
+        store_id,
+        order_status,
+        total_amount,
+        _synced_at
     from {{ ref('stg_ecomm__orders') }}
     {% if is_incremental() %}
         where ordered_at >= (select dateadd('day', -3, max(ordered_at)) from {{ this }})
@@ -15,7 +22,12 @@ with orders as (
 ),
 
 deliveries as (
-    select *
+    select
+        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as pk_orders,
+        delivered_at,
+        picked_up_at,
+        _synced_at,
+        delivery_status
     from {{ ref('stg_ecomm__deliveries') }}
 ),
 
@@ -32,12 +44,16 @@ stores as (
 
 joined as (
     select
-        orders.order_id,
-        orders.customer_id,
+        orders.pk_orders,
+        orders.hk_customer,
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        orders.store_id,
         stores.store_name,
+        greatest_ignore_nulls(
+            orders._synced_at, deliveries_filtered._synced_at
+        ) as source_last_updated,
         current_timestamp() as last_updated,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
@@ -50,14 +66,14 @@ joined as (
           datediff(
             'day',
             lag(ordered_at) over (
-            partition by customer_id
+            partition by hk_customer
             order by ordered_at
             ),
             ordered_at
         ) as days_since_last_order
     from orders
     left join deliveries_filtered
-        on orders.order_id = deliveries_filtered.order_id
+        on orders.pk_orders = deliveries_filtered.pk_orders
     left join stores
         on orders.store_id = stores.store_id
 ),
