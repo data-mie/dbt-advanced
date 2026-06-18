f96eaba2813afad991e6ca8dda85d9299bbf9290
Patricia Oliveira <patricia@breakout-labs.com>
2026-06-18 16:36:42 +0100

    Improve renaming CTE for stg_ecomm__orders due to lab misunderstaging

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -5,10 +5,13 @@ with source as (
 
 renamed as (
     select
-        *,
         id as order_id,
+        total_amount,
+        status as order_status,
         created_at as ordered_at,
-        status as order_status
+        customer_id,
+        store_id,
+        _synced_at
     from source
 ),
 
